eb9bf2f6e2e148a2fcf889f68086f59453518795
Kimberly Perez <kperez22692@gmail.com>
2021-07-15 22:21:13 -0400

    update

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var search = document.getElementsByTagName("form"[0].innerHTML);
-var history = $('#history');
+var search = $("#searchcity");
+var historycities = $('#history');
 var weatherApi = "pro.openweathermap.org/data/2.5/forecast/hourly?";
 var apiKey = "&APPID=844097de582cb0e70050fb6a41b3baf2";
 var units = "&units=metric";
 
+
 function handleFormSubmit(event) {
     event.preventDefault();
-
+    console.log("this is working!");
     var city = $('input[name="search-input"]').val();
 
     if (!city) {
@@ -14,35 +15,34 @@ function handleFormSubmit(event) {
         return;
     }
 
-    history.append('<li>' + city + '</li>');
+    historycities.append('<li>' + city + '</li>');
 
     $('input[name="search-input"]').val('');
+    getApi();
 }
 
-search.on('submit', handleFormSubmit);
 
 function getApi() {
-    // replace `octocat` with anyone else's GitHub username
-    var requestUrl = 'https://pro.openweathermap.org/data/2.5/forecast/hourly?' + '&APPID=844097de582cb0e70050fb6a41b3baf2' + '&units=metric';
+  // replace `octocat` with anyone else's GitHub username
+  var requestUrl = 'https://pro.openweathermap.org/data/2.5/forecast/hourly?' + '&APPID=844097de582cb0e70050fb6a41b3baf2' + '&units=metric';
   
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-          var listItem = document.createElement('li');
-          listItem.textContent = data[i].html_url;
-          repoList.appendChild(listItem);
-        }
-      });
-  }
-  
-  fetchButton.addEventListener('click', getApi);
+  fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    for (var i = 0; i < data.length; i++) {
+      var listItem = document.createElement('li');
+      listItem.textContent = data[i].html_url;
+      repoList.appendChild(listItem);
+    }
+  });
+}
+
+search.on('click', handleFormSubmit);
 
   function forecast(event) {
     event.preventDefault();
-    console.log();
     var button = select('.button expanded');
     button.mousePressed(weatherask);
     input = select('.searchCities');
@@ -58,7 +58,7 @@ function getApi() {
     weatherApi
   }
   
-  
+
   function searchForecast(event) {
     //prevent default refresh
     event.preventDefault();
@@ -78,12 +78,12 @@ function getApi() {
   //need a function to display search history and clear current search
   
   //when search button is clicked...
-  search.addEventListener("click", searchForecast);
+  // search.addEventListener("click", searchForecast);
   
-  var searchBar = document.getElementById("searchBar");
+  // var searchBar = document.getElementById("searchBar");
   
-  searchBar.addEventListener("click", function(){
-      console.log("this is working");
-      // url = Cultures.Arab;
-      // console.log(idNumber)
-  });
+  // searchBar.addEventListener("click", function(){
+  //     console.log("this is working");
+  //     // url = Cultures.Arab;
+  //     // console.log(idNumber)
+  // });
